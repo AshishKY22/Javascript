745fbb253f3adb517e6fb555d235b0d0df630dbe
Ashish Kumar Yadav <ak9749120791@gmail.com>
2026-06-26 16:53:42 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);//> < >= <= → null gets converted to 0
+console.log(null == 0);// == → null has special equality rules (only equals undefined)
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);//wont does the conversion of the types
